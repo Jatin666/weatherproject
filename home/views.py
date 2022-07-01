e51fee707dfcbcf9f81889cab57cdf48e94d2cf4
Jatin Kant <59907081+Jatin666@users.noreply.github.com>
2022-07-01 10:57:50 +0200

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
 import requests
-# Create your views here.
 
 def home(request):
-    city = requests.GET.get('city', 'Delhi')
-    #city ="Delhi"
+    city = request.GET.get('city')
+    print(city)
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=ef30ebca6ebcab61e960146be2b18900"
     data = requests.get(url).json()
-   # print(data)
-
-    payload = {'city': data['name'],
-             'weather': data['weather'][0]['main'],
-             'icon': data['weather'][0]['icon'],
-             'kelvin_temperature': data['main']['temp'],
-             'celcius_temperature': data['main']['temp'] - 273,
-             'pressure':data['main']['pressure'],
-             'humidity':data['main']['humidity'],
-             'description':data['weather'][0]['description']
-             }
+    if data['cod'] != "404":
+        payload = {'city': data['name'],
+                 'weather': data['weather'][0]['main'],
+                 'icon': data['weather'][0]['icon'],
+                 'kelvin_temperature': data['main']['temp'],
+                 'celcius_temperature': data['main']['temp'] - 273,
+                 'pressure':data['main']['pressure'],
+                 'humidity':data['main']['humidity'],
+                 'description':data['weather'][0]['description']
+                 }
+    else:
+        payload=data
     context ={'data': payload}
-    print(context)
-    return render(request,'home.html')
+    return render(request,'home.html', context)
